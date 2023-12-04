a3001d25dd97a053923e9e96fce13e760da0de1f
NelsonProv <120102286+NelsonProv@users.noreply.github.com>
2023-12-04 15:27:51 +0200

    testing changes

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,8 @@
-REM Build your React application
-npm run build
+@REM REM Build your React application
+@REM npm run build
 
 REM Copy the built files to the NGINX html folder
 xcopy /S /Y build\* C:\nginx-1.24.0\html
 
-REM Restart NGINX (adjust the command based on your server setup)
-C:\nginx-1.24.0\nginx.exe -s reload
+@REM REM Restart NGINX (adjust the command based on your server setup)
+@REM C:\nginx-1.24.0\nginx.exe -s reload
