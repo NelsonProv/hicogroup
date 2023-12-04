5e12c74ad696717a6d347310b537f882da51e0e7
NelsonProv <120102286+NelsonProv@users.noreply.github.com>
2023-12-04 15:24:13 +0200

    testing changes

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 REM Copy the built files to the NGINX html folder
 xcopy /S /Y build\* C:\nginx-1.24.0\html
 
-REM Restart NGINX (adjust the command based on your server setup)
-C:\nginx-1.24.0\nginx.exe -s reload
+@REM REM Restart NGINX (adjust the command based on your server setup)
+@REM C:\nginx-1.24.0\nginx.exe -s reload
